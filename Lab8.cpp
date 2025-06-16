e766e5e457f08cb08a665ed934b492dd09096147
Gary9864 <00021925@uca.edu.sv>
2025-06-16 08:40:23 -0600

    funcion

--- Lab8.cpp
+++ Lab8.cpp
@@ -1,32 +1,32 @@
-#include <iostream>//pedir un numero al usuario y por medio de una funcion determinar si es perfecto,la suma de sus divisores es igual al numero
+#include <iostream>
 using namespace std;
-int numero_perfecto(int n1){
-   int divisores = 0;
-   for (int i = 1; i < n1 ; i++)
+int numero_perfecto(int n1){ //la funcion para determinar si es perfecto o no
+   int divisores = 0;// Quemo la variable en 0
+   for (int i = 1; i < n1 ; i++) //determinar todos los divisores
    {
     if (n1 % i == 0)
     {
-        divisores +=i;
+        divisores +=i;// sumar los divisores
     }
     
    }
    
     
     
-    return divisores == n1 ;
+    return divisores == n1 ;//los divisores seran acorde al numero que de el usuario
 }
 
 
 int main(){
-    int numero;
+    int numero;// numero que da el usuario
     cout << "Ingresa un numero" << endl;
     cin >> numero;
 
-    if (numero_perfecto(numero))
+    if (numero_perfecto(numero))// la funcion para numero perfecto con el numero
     {
-        cout << "Es un numero perfecto"<< endl;
+        cout << "Es un numero perfecto"<< endl;// si se cumple que es perfecto
     }else{
-        cout << "No es numero perfecto"<< endl;
+        cout << "No es numero perfecto"<< endl;// no se cumple
     }
     
     return 0;
